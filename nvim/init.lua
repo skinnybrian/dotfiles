c75656fad79510e64b3f507fd82fa23efb01004e
skinnybrian <post@skinnybrian.com>
2023-12-27 15:13:24 +0900

    Set encoding, options, and keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,20 @@
+local opt = vim.opt
+
+-- Encoding
+vim.scriptencoding = 'utf-8'
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
 -- Options
-vim.opt.swapfile = false
-vim.opt.number = true
-vim.opt.cursorline = true
+opt.swapfile = false
+opt.backup = false
+opt.hidden = true
+opt.number = true
+opt.cursorline = true
+opt.shortmess:append({ I = true })
+opt.clipboard = 'unnamed'
+opt.list = true
+opt.listchars = { tab = '»-', trail = '·', eol = '¬', extends = '»', precedes = '«', nbsp = '%'}
 
 -- Keymap
 vim.keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true})
