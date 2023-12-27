9d9f7924854ea50b42e58b46a6a4edb1736ef396
skinnybrian <post@skinnybrian.com>
2023-12-27 13:07:02 +0900

    Add options and keymap in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,7 @@
-print ('init lua')
+-- Options
+vim.opt.swapfile = false
+vim.opt.number = true
+vim.opt.cursorline = true
+
+-- Keymap
+vim.keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true})
