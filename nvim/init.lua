46395116b3b181d3af7b1db07a0f7d24f49e62d9
skinnybrian <post@skinnybrian.com>
2023-12-27 15:29:08 +0900

    Refactor keymap in init.lua for better readability and maintainability

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 local opt = vim.opt
+local keymap = vim.keymap
 
 -- Encoding
 vim.scriptencoding = 'utf-8'
@@ -14,7 +15,11 @@ opt.cursorline = true
 opt.shortmess:append({ I = true })
 opt.clipboard = 'unnamed'
 opt.list = true
-opt.listchars = { tab = '»-', trail = '·', eol = '¬', extends = '»', precedes = '«', nbsp = '%'}
+opt.listchars = {tab = '»-', trail = '·', eol = '¬', extends = '»', precedes = '«', nbsp = '%'}
+opt.tabstop = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.shiftwidth = 4
 
 -- Keymap
-vim.keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true})
+keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true})
