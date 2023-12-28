fcb988a6cd3a3dbc8fceae5cf0219792238ba6cc
skinnybrian <post@skinnybrian.com>
2023-12-28 10:28:36 +0900

    Update clipboard option in init.lua to include 'unnamedplus' and add lazy.nvim plugin to nvim configuration.Optimize clipboard and add lazy.nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,7 @@ opt.hidden = true
 opt.number = true
 opt.cursorline = true
 opt.shortmess:append({ I = true })
-opt.clipboard = 'unnamed'
+opt.clipboard:append({ 'unnamedplus' })
 opt.list = true
 opt.listchars = {tab = '»-', trail = '·', eol = '¬', extends = '»', precedes = '«', nbsp = '%'}
 opt.tabstop = 4
@@ -23,8 +23,22 @@ opt.shiftwidth = 4
 
 -- Keymap
 keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true})
-
 keymap.set('', '<Up>', '<nop>')
 keymap.set('', '<Down>', '<nop>')
 keymap.set('', '<Left>', '<nop>')
 keymap.set('', '<Right>', '<nop>')
+
+-- Plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(plugins, opts)
