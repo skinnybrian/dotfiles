a72bbdb276db111a3967487f2a685cbfcdace07b
skinnybrian <post@skinnybrian.com>
2023-12-31 16:36:17 +0900

    Add eslint autofix on save and configure filetypes for eslint in coc settings. Also, add keymap for showing documentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -243,3 +243,15 @@ end, {remap=true})
 vim.keymap.set('', 'T', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, {remap=true})
+
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
