c8c0e9c3ef4550d8aedb0e819b1c09e16296970c
skinnybrian <post@skinnybrian.com>
2023-12-31 11:16:37 +0900

    Add phaazon/hop.nvim to lazy plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -129,7 +129,10 @@ require("lazy").setup({
         indent = { enable = true },
       })
     end
-  }
+  },
+  {
+    'phaazon/hop.nvim'
+  },
 })
 
 -- Theme
@@ -225,3 +228,4 @@ require('ts_context_commentstring').setup {
 require('Comment').setup {
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 }
+require'hop'.setup()
