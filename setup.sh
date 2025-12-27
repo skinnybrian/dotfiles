d5874a240a4402ce64123077459248a48befa0d0
Shinnosuke HORIGUCHI <brian@ShinnosukenoMacBook-Pro.local>
2025-12-27 23:04:12 +0900

    add setup.sh ghostty symbolic link

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/ghostty ~/.config/ghostty
